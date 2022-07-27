ab38920b34b6abdd5364992ba1948c966314df20
Lincoln Arrais <lincoln_arrais@hotmail.com>
2022-07-26 22:02:41 -0300

    aula 12

--- lib/backend.dart
+++ lib/backend.dart
@@ -6,6 +6,24 @@ import 'src/app_module.dart';
 startShelfModular() {
   final handler = Modular(module: AppModule(), middlewares: [
     logRequests(),
+    jsonResponse(),
   ]);
   return handler;
+}
+
+Middleware jsonResponse() {
+  return (handler) {
+    return (request) async {
+      var response = await handler(request);
+
+      response = response.change(
+        headers: {
+          'content-type': 'application/json',
+          ...response.headers,
+        },
+      );
+
+      return response;
+    };
+  };
 }
